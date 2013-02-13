f225b5473cd9bf972655e95bdd821f3b70128bb2
Allen Greaves <allen.w.greaves@gmail.com>
2013-02-12 18:38:09 -0800

    Added a console application to monitor star data.

---
+++ src/StarDataMonitor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SampleIntegrationPoint;
+
+namespace StarDataMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StarDataPump.Seed = 1;
+            StarDataPump.NewStarFound +=
+                    ( sender, eventArgs ) =>
+                    Console.WriteLine( "{0}\t{1}", eventArgs.Star.StarType, eventArgs.Star.Mass );
+            StarDataPump.Start();
+            while ( true )
+            {
+            }
+        }
+    }
+}
