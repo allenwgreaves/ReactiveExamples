9f940fc445d60185d537babdd14379dea3443e1a
Allen Greaves <allen.w.greaves@gmail.com>
2013-02-12 16:34:42 -0800

    Added initial sources and created a test.

---
+++ src/SampleReactive.Tests/SampleTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleReactive.Tests
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void ToObservable()
+        {
+            IEnumerable<int> items = new List<int> { 121, 242, 383 };
+            IObservable<int> observableItems = items.ToObservable();
+            Assert.IsInstanceOfType( observableItems, typeof(IObservable<int>) );
+        }
+    }
+}
