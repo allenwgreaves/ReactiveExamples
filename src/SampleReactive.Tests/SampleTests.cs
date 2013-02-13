a22c1a836fac3e6b1e3b0ce6fb1476c3f28e2899
Allen Greaves <allen.w.greaves@gmail.com>
2013-02-12 16:50:22 -0800

    Added Subscribe tests.

--- src/SampleReactive.Tests/SampleTests.cs
+++ src/SampleReactive.Tests/SampleTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SampleReactive.Tests
@@ -14,6 +16,25 @@ namespace SampleReactive.Tests
             IEnumerable<int> items = new List<int> { 121, 242, 383 };
             IObservable<int> observableItems = items.ToObservable();
             Assert.IsInstanceOfType( observableItems, typeof(IObservable<int>) );
+        }
+
+        [TestMethod]
+        public void ToObservable_Subscribe_RunsOnTheCurrentThread()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            IEnumerable<int> items = new List<int> { 121, 242, 383 };
+            items.ToObservable().Subscribe(x => Assert.AreEqual<int>(currentThreadId, Thread.CurrentThread.ManagedThreadId) );
+        }
+
+        [TestMethod]
+        public void ToObserverableWithSchedulerDefault_Subscribe_RunsOnADifferentThread()
+        {
+            AutoResetEvent @event = new AutoResetEvent( false );
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            IEnumerable<int> items = new List<int> { 121, 242, 383 };
+            items.ToObservable( Scheduler.Default ).Subscribe( x => Assert.AreNotEqual<int>( currentThreadId, Thread.CurrentThread.ManagedThreadId ),
+            () => @event.Set());
+            @event.WaitOne();
         }
     }
 }
